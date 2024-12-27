3beb0ceb803d406611ec46828f5eefcb217bcd4c
Wes Benvindo <wal2.designer@gmail.com>
2024-12-27 21:01:20 +0000

    Connected with dabtabase

--- ERP_INES/Data/InesDbContext.cs
+++ ERP_INES/Data/InesDbContext.cs
@@ -1,8 +1,13 @@
+using ERP_INES.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace ERP_INES.Data;
 
 public class InesDbContext : DbContext
 {
-    
+    public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql("Host=localhost:5432;DataBase=ines_erp;Username=postgres;Password=postgres");
 }
--- ERP_INES/Program.cs
+++ ERP_INES/Program.cs
@@ -1,7 +1,16 @@
+using ERP_INES.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+// builder.Services.AddDbContextPool<InesDbContext>(opt =>
+//     opt.UseNpgsql(
+//         builder.Configuration.GetConnectionString("Development"),
+//         o => o.SetPostgresVersion(17,2)
+//         ));
 
 var app = builder.Build();
 

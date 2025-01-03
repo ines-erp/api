35151c2f6b9103d1e29cc421472877770a499cab
Bruna <brn.flor@gmail.com>
2025-01-03 18:44:48 +0000

    Removing optional description from dto

--- ERP_INES/Domain/Modules/Finance/DTOs/UpdatePaymentMethodDto.cs
+++ ERP_INES/Domain/Modules/Finance/DTOs/UpdatePaymentMethodDto.cs
@@ -3,5 +3,5 @@ namespace ERP_INES.Domain.Modules.Finance.DTOs;
 public class UpdatePaymentMethodDto
 {
     public string Name { get; set; }
-    public string? Description { get; set; }
+    public string Description { get; set; }
 }
